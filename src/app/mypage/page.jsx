637d8ce4b01a3d12ef2adfb2973b3f4c8fccbebf
Koran1 <thekorani@gmail.com>
2025-01-03 09:57:00 +0900

    250103 faq, notice, mypage, layout.js 우선 복구

--- src/app/mypage/page.jsx
+++ src/app/mypage/page.jsx
@@ -2,13 +2,32 @@
 import React, { useEffect } from 'react';
 import MyPageList from './MyPageList';
 import { Box } from '@mui/material';
+import useAuthStore from '../../../store/authStore';
+import { useRouter } from 'next/navigation';
 
 function Page() {
+    // 로그인 확인
+    const router = useRouter();
+    const { isAuthenticated, isExpired, user } = useAuthStore();
+
+    useEffect(() => {
+        if (!user) return
+        console.log('유저 로그인 확인')
+        if (!isAuthenticated || isExpired()) {
+            alert("로그인이 필요한 서비스입니다.");
+            router.push("/user/login"); // Redirect to login page
+            return
+        }
+
+    }, [user])
 
     return (
-        <div>
-            <h2>This is MyPage</h2>
-        </div>
+        <Box display='flex'>
+            <MyPageList />
+            <Box flexGrow={1} p={2} m={1} sx={{ border: '1px solid black' }}>
+                <h2>This is MyPage Main</h2>
+            </Box>
+        </Box>
     );
 }
 
